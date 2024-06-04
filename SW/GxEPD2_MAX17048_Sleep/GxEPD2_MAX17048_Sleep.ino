32740e7952068e20cb604f8a9a7b1ff6716ab660
Konstantin Láska <konstantin.laska@icloud.com>
2024-06-04 12:22:54 +0200

    update to S3

---
+++ SW/GxEPD2_MAX17048_Sleep/GxEPD2_MAX17048_Sleep.ino
@@ -0,0 +1,130 @@
+// Display Library example for SPI e-paper panels from Dalian Good Display and boards from Waveshare.
+// Requires HW SPI and Adafruit_GFX. Caution: the e-paper panels require 3.3V supply AND data lines!
+//
+// Display Library based on Demo Example from Good Display: http://www.e-paper-display.com/download_list/downloadcategoryid=34&isMode=false.html
+//
+// Author: Jean-Marc Zingg
+//
+// Version: see library.properties
+//
+// Library: https://github.com/ZinggJM/GxEPD2
+
+// Supporting Arduino Forum Topics:
+// Waveshare e-paper displays with SPI: http://forum.arduino.cc/index.php?topic=487007.0
+// Good Display ePaper for Arduino: https://forum.arduino.cc/index.php?topic=436411.0
+
+// see GxEPD2_wiring_examples.h for wiring suggestions and examples
+
+// base class GxEPD2_GFX can be used to pass references or pointers to the display instance as parameter, uses ~1.2k more code
+// enable or disable GxEPD2_GFX base class
+#define ENABLE_GxEPD2_GFX 0
+
+// uncomment next line to use class GFX of library GFX_Root instead of Adafruit_GFX
+//#include <GFX.h>
+// Note: if you use this with ENABLE_GxEPD2_GFX 1:
+//       uncomment it in GxEPD2_GFX.h too, or add #include <GFX.h> before any #include <GxEPD2_GFX.h>
+
+#include <GxEPD2_BW.h>
+#include <GxEPD2_3C.h>
+#include <GxEPD2_7C.h>
+#include <Adafruit_MAX1704X.h>
+
+#include <Fonts/FreeMonoBold9pt7b.h>
+
+#include "bitmaps/Bitmaps1304x984.h" // 12.48" b/w
+
+#define SLEEP_SEC 15         // Measurement interval (seconds)
+
+Adafruit_MAX17048 maxlipo;
+
+GxEPD2_BW<GxEPD2_420_GDEY042T81, GxEPD2_420_GDEY042T81::HEIGHT> display(GxEPD2_420_GDEY042T81(/*CS=5*/ SS, /*DC=*/ 17, /*RST=*/ 16, /*BUSY=*/ 4)); // GDEY042T81, 400x300, SSD1683 (no inking)
+//GxEPD2_3C<GxEPD2_420c_Z21, GxEPD2_420c_Z21::HEIGHT> display(GxEPD2_420c_Z21(/*CS=5*/ SS, /*DC=*/ 17, /*RST=*/ 16, /*BUSY=*/ 4)); // GDEQ042Z21 400x300, UC8276
+
+void setup()
+{
+  Serial.begin(115200);
+  Serial.println();
+  Serial.println("setup");
+  delay(100);
+
+// turn on power to display
+  pinMode(2, OUTPUT);
+  digitalWrite(2, HIGH);   // turn the LED on (HIGH is the voltage level)
+  Serial.println("Display power ON");
+  delay(1000);  
+
+  maxlipo.wake();
+  while (!maxlipo.begin()) {
+    Serial.println(F("Couldnt find MAX17048?\nMake sure a battery is plugged in!"));
+    delay(2000);
+  }
+  Serial.print(F("Found MAX17048"));
+  Serial.print(F(" with Chip ID: 0x")); 
+  Serial.println(maxlipo.getChipID(), HEX);
+
+  display.init(); // inicializace
+
+  float cellVoltage = maxlipo.cellVoltage();
+  if (isnan(cellVoltage)) {
+    Serial.println("Failed to read cell voltage, check battery is connected!");
+    delay(2000);
+    return;
+  }
+  Serial.print(F("Batt Voltage: ")); Serial.print(cellVoltage, 3); Serial.println(" V");
+  Serial.print(F("Batt Percent: ")); Serial.print(maxlipo.cellPercent(), 1); Serial.println(" %");
+  Serial.println();
+
+  // first update should be full refresh
+  helloWorld();
+  delay(1000);
+
+  display.powerOff();
+
+  Serial.println("setup done");
+
+  goToSleep();
+}
+
+void goToSleep(){
+  Serial.println("going to sleep " + String(SLEEP_SEC) + " sek");
+
+  maxlipo.hibernate();
+
+  // ESP Deep Sleep 
+  digitalWrite(2, LOW);   // turn the LED on (HIGH is the voltage level)
+  Serial.println("Display power OFF");
+  Serial.println("ESP in sleep mode");
+  Serial.flush(); 
+  esp_sleep_enable_timer_wakeup(SLEEP_SEC * 1000000);
+  esp_deep_sleep_start();
+}
+
+void loop()
+{
+}
+
+const char HelloWorld[] = "Hello World!";
+
+void helloWorld()
+{
+  //Serial.println("helloWorld");
+  display.setRotation(1);
+  display.setFont(&FreeMonoBold9pt7b);
+  if (display.epd2.WIDTH < 104) display.setFont(0);
+  display.setTextColor(GxEPD_BLACK);
+  int16_t tbx, tby; uint16_t tbw, tbh;
+  display.getTextBounds(HelloWorld, 0, 0, &tbx, &tby, &tbw, &tbh);
+  // center bounding box by transposition of origin:
+  uint16_t x = ((display.width() - tbw) / 2) - tbx;
+  uint16_t y = ((display.height() - tbh) / 2) - tby;
+  display.setFullWindow();
+  display.firstPage();
+  do
+  {
+    display.fillScreen(GxEPD_WHITE);
+    display.setCursor(x, y);
+    display.print(HelloWorld);
+  }
+  while (display.nextPage());
+  Serial.println("helloWorld done");
+}
